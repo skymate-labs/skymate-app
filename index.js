8e8b5d2ac5640aecf9e64890a95ddb26ef85dc4c
tera <tera@skymate.life>
2023-10-04 01:44:32 +0700

    feat: Update structure

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import 'react-native-get-random-values';
 import 'react-native-url-polyfill/auto';
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './src/App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
